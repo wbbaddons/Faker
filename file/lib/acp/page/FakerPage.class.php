b05436e974b17b9d362edd2d35e8716bb707b6fc
Tim Düsterhus <timwolla@arcor.de>
2013-03-03 21:03:41 +0100

    Add frontend barebone

---
+++ file/lib/acp/page/FakerPage.class.php
@@ -0,0 +1,15 @@
+<?php
+namespace wcf\acp\page;
+
+/**
+ * Shows faker page.
+ *
+ * @author	Tim Düsterhus
+ * @copyright	2013 Tim Düsterhus
+ * @license	Creative Commons Attribution-NonCommercial-ShareAlike <http://creativecommons.org/licenses/by-nc-sa/3.0/legalcode>
+ * @package	be.bastelstu.wcf.faker
+ * @subpackage	acp.page
+ */
+class FakerPage extends \wcf\page\AbstractPage {
+	
+}
