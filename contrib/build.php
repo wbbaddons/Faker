3eeb89dbb8ab9997b2f48439dea8cebf1e3d918e
Tim Düsterhus <timwolla@arcor.de>
2013-05-01 18:07:04 +0200

    Move buildscript to contrib folder

--- contrib/build.php
+++ contrib/build.php
@@ -65,7 +65,7 @@ Building file.tar
 -----------------
 
 EOT;
-	passthru('tar cvf ../file.tar * --exclude=.git', $code);
+	passthru('tar cvf ../file.tar --exclude=.git -- *', $code);
 	if ($code != 0) exit($code);
 echo <<<EOT
 
@@ -86,7 +86,7 @@ EOT;
 	chdir('..');
 	file_put_contents('package.xml.old', file_get_contents('package.xml'));
 	file_put_contents('package.xml', preg_replace('~<date>\d{4}-\d{2}-\d{2}</date>~', '<date>'.date('Y-m-d').'</date>', file_get_contents('package.xml')));
-	passthru('tar cvf be.bastelstu.wcf.faker.tar * --exclude=*.old --exclude=file --exclude=build.php', $code);
+	passthru('tar cvf be.bastelstu.wcf.faker.tar --exclude=*.old --exclude=file --exclude=contrib -- *', $code);
 	if (file_exists('package.xml.old')) {
 		file_put_contents('package.xml', file_get_contents('package.xml.old'));
 		unlink('package.xml.old');
