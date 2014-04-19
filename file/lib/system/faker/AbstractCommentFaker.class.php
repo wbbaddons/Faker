47628b504ebf3d3f83ae6ba19916bef0026385d3
Matthias Schmidt <gravatronics@live.com>
2014-04-19 19:20:20 +0200

    Fixes comment and wall faker if only one user exists

--- file/lib/system/faker/AbstractCommentFaker.class.php
+++ file/lib/system/faker/AbstractCommentFaker.class.php
@@ -56,7 +56,7 @@ abstract class AbstractCommentFaker extends AbstractFaker {
 		$sql = "SELECT		userID, username, registrationDate
 			FROM		wcf".WCF_N."_user
 			ORDER BY	userID ASC";
-		$statement = \wcf\system\WCF::getDB()->prepareStatement($sql, 1, $this->generator->numberBetween(0, $this->userCount - 1));
+		$statement = \wcf\system\WCF::getDB()->prepareStatement($sql, 1, $this->userCount - 1 ? $this->generator->numberBetween(0, $this->userCount - 1) : 0);
 		$statement->execute();
 		$sender = $statement->fetchObject('\wcf\data\user\User');
 		
